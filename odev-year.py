d34ad9c2841570afd2c1352bedcdeaf53e8a292a
mulani-web <81701864+mulani-web@users.noreply.github.com>
2022-06-16 13:52:30 +0530

    Add files via upload

---
+++ odev-year.py
@@ -0,0 +1,14 @@
+def oddeven(a):
+   if n%2 ==0:
+    print("number is even")
+   else:
+    print("number is odd")
+def year(a):
+   if n % 4 ==0:
+    print("it is a leap year")
+   else:
+    print("normal year")
+n = int(input("enter the number:"))
+oddeven(n)
+n = int(input("enter the year:"))
+year(n)    
