cf14178e8033a16e008e49a0c75eefcb14f9773d
mulani-web <81701864+mulani-web@users.noreply.github.com>
2022-06-17 13:08:52 +0530

    Add files via upload

---
+++ swapnum.py
@@ -0,0 +1,11 @@
+from re import M
+
+
+n = int(input("enter the value of n:"))
+m = int(input("enter the value of m:"))
+x = n
+n = m 
+m = x
+print("after swapping")
+print("value of n ",n)
+print("value of m ",m)
